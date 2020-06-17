c381b3f72f57d45cbcdb1d612fc815d4889d093b
unknown <ludvig_olausson@hotmail.com>
2020-06-18 01:45:28 +0200

    Ajax brands

--- public/functions.php
+++ public/functions.php
@@ -1,15 +1,16 @@
 <?php
     require('../src/dbconnect.php');
     //require(SRC_PATH . 'dbconnect.php');
-    error_reporting(0);
+    error_reporting(1);
 
     function writeProd($conn, $select, $from, $where, $like, $like2, $like3) {
         if($like == "" || $like == null && $like2 == null && $like3 == null){
             $sth = $conn->prepare("SELECT $select FROM $from WHERE $where");
         }else{
-            $sth = $conn->prepare("SELECT $select FROM $from WHERE $where LIKE $like OR $where LIKE $like2 OR $where LIKE $like3");
+          //  $sth = $conn->prepare("SELECT $select FROM $from WHERE $where LIKE $like OR $like2 OR $like3");
+          $sth = $conn->prepare("SELECT $select FROM $from WHERE $where LIKE $like OR $where LIKE $like2 OR $where LIKE $like3");
+          //echo "SELECT $select FROM $from WHERE $where LIKE $like OR $like2 OR $like3";
         }
-        
         $sth->execute();
         /* Fetch all of the remaining rows in the result set */
        // print("Fetch all of the remaining rows in the result set:\n");
@@ -23,8 +24,11 @@
         echo writeProd($dbconnect, "`id`, `title`, `description`, `price`, `img_url`", "`products`", "1", null, null, null);
     }elseif($_GET['data'] == "rec_prod"){
         echo writeProd($dbconnect, "`id`, `title`, `description`, `price`, `img_url`", "`products`", "`title`", "'%oneplus%'", "'%iphone%'", "'%NOKIA%'");
+    }elseif($_GET['data'] == "front_brand"){
+        $brandFilter = $_GET['filter'];
+        echo writeProd($dbconnect, "`id`, `title`, `description`, `price`, `img_url`", "`products`", "`title`", "'%" . $brandFilter . "%'", "NULL", "NULL");
     }
 
-
+//SELECT `id`,`title`,`description`,`price`,`img_url` FROM `products` WHERE `title` LIKE '%oneplus%' OR NULL OR NULL
 ?>
 
--- public/js/Ajax.call.js
+++ public/js/Ajax.call.js
@@ -44,3 +44,25 @@ function reco_front_page_ajax(){
           
       });
 }
+function brandsAjaxProducts(filter){
+  //$( "._procontainer .Item" ).hide();
+  let baseUrl = 'http://localhost/E-commerce/public/functions.php';
+  $.get(baseUrl + '?data=' + "front_brand" + "&filter=" + filter, function(response) {
+
+              let req = response;
+            //  $.each(req, function(index) {
+                  for(let index=0; index<12; index++){
+                  var t = $("<div class=Item>" +
+                  "<div class='cont' onclick='window.location.href=\'product-page.php\''>" +
+                  "<div class='img_c'><img src='" + req[index].img_url + "' width='30%'></div>" +
+                  "<div class='brand_n'>Brand</div>" +
+                  "<div class='title'>"+ req[index].title +"</div>" +
+                  "<div class='prize'>"+ req[index].price +"kr</div></div></div>");
+                  $("._procontainer").append(t).show('slow');;
+                };
+          },
+          'json'
+      ).fail(function() {
+          
+      });
+}
