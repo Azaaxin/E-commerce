581cfd11022144b2a247b59ec7e18e9466923dfa
unknown <ludvig_olausson@hotmail.com>
2020-06-23 01:29:06 +0200

    auto edit, search, list products, html supported edit with paste

--- public/functions.php
+++ public/functions.php
@@ -2,6 +2,22 @@
     require('../src/dbconnect.php');
     error_reporting(1);
 
+
+
+    function editProd($conn){
+        $edit_title = htmlspecialchars($_POST['title']);
+        $edit_desc = $_POST['desc'];
+        $edit_price = htmlspecialchars($_POST['price']);
+        $edit_img = htmlspecialchars($_POST['img']);
+        $edit_id = htmlspecialchars($_POST['id']);
+
+        $sth = $conn->prepare("UPDATE `products` SET `title` = '$edit_title', `description` = '$edit_desc', `price` = '$edit_price', `img_url` = '$edit_img'  WHERE `id` = '$edit_id';");
+        $sth->execute();
+    }
+    if($_GET['edit']=="true"){
+        editProd($dbconnect);
+    }
+
     function writeProd($conn, $select, $from, $where, $like, $like2, $like3) {
         if($like == "" || $like == null && $like2 == null && $like3 == null){
             $sth = $conn->prepare("SELECT $select FROM $from WHERE $where");
@@ -24,8 +40,13 @@
         $id = $_GET['id'];
         $brandFilter = $_GET['filter'];
         echo writeProd($dbconnect, "`id`, `title`, `description`, `price`, `img_url`", "`products`", "`id`=". $id ."", null, null, null);
+    }elseif($_GET['data'] == "search"){
+        $search = $_GET['search'];
+        $brandFilter = $_GET['filter'];
+        echo writeProd($dbconnect, "`id`, `title`, `description`, `price`, `img_url`", "`products`", "`title`", "'%" . $search . "%'", "NULL", "NULL");
     }
     
+
 
 ?>
 
