35cd2d4954fead8028d6d517b63acd57523125bf
annisjr <60426738+annisjr@users.noreply.github.com>
2020-06-25 10:35:19 +0200

    Add files via upload

---
+++ public/admin/add_user.php
@@ -0,0 +1,154 @@
+<?php  
+    session_start();
+    require('../../src/dbconnect.php');
+
+
+    $first_name   = '';
+    $last_name    = '';
+    $email        = '';
+    $phone        = '';
+    $street       = '';
+    $postal_code  = '';
+    $city         = '';
+    $country      = '';
+    $msg          = '';
+    if (isset($_POST['register'])) {
+        $first_name        = trim($_POST['first_name']);
+        $last_name         = trim($_POST['last_name']);
+        $email             = trim($_POST['email']);
+        $password          = trim($_POST['password']);
+        $phone             = trim($_POST['phone']);
+        $street            = trim($_POST['street']);
+        $postal_code       = trim($_POST['postal_code']);
+        $city              = trim($_POST['city']);
+        $country           = trim($_POST['country']);
+
+        if (empty($first_name)) {
+            $error .= "<li>Förnamn är obligatoriskt</li>";
+        }
+        if (empty($last_name)) {
+            $error .= "<li>Efternamn är obligatoriskt</li>";
+        }
+        if (empty($email)) {
+            $error .= "<li>Email är obligatoriskt</li>";
+        }
+
+        if (empty($password)) {
+            $error .= "<li>Lösenord är obligatoriskt</li>";
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $error .= "<li>Ogiltig e-post</li>";
+        }
+        
+
+        if ($error) {
+            $msg = "<ul class='error_msg bg-danger'>{$error}</ul>";
+        }
+
+        if (empty($error)) {
+            try {
+                $query = "
+                    INSERT INTO users (first_name, last_name, email, password, phone, street, postal_code, city, country)
+                    VALUES (:first_name, :last_name, :email, :password, :phone, :street, :postal_code, :city, :country);
+                ";
+
+                $stmt = $dbconnect->prepare($query);
+                $stmt->bindValue(':first_name', $first_name);
+                $stmt->bindValue(':last_name', $last_name);
+                $stmt->bindValue(':email', $email);
+                $stmt->bindValue(':password', $password);
+                $stmt->bindValue(':phone', $phone);
+                $stmt->bindValue(':street', $street);
+                $stmt->bindValue(':postal_code', $postal_code);
+                $stmt->bindValue(':city', $city);
+                $stmt->bindValue(':country', $country);
+                $result = $stmt->execute(); // returns true/false
+            } catch(\PDOException $e) {
+                throw new \PDOException($e->getMessage(), (int) $e->getCode());
+            }
+
+            if ($result) {
+                $msg = '<div class="success_msg bg-success">Ny användare är skapad</div>';
+            } else {
+                $msg = '<div class="error_msg bg-danger">Skapandet av användaren misslyckades. Var snäll och försök igen senare!</div>';
+            }
+        }
+    }       
+
+
+
+?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Add user</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    </head>
+    <body>
+        <h3 style="text-align: center;">Skapa Användare</h3>
+            <div class="row justify-content-center">
+                <div class="col-6">
+                
+                
+                    <form method="POST" action="#" id="addUser-form">
+                        <fieldset>
+
+                          <?=$msg?>
+
+                          <div id="form-row" class="form-row">
+                            <div class="col-6">
+                              <input type="text" class="form-control" name="first_name" placeholder="Förnamn" value="<?=htmlentities($first_name)?>">
+                            </div>
+                            <div class="col-6">
+                              <input type="text" class="form-control" name="last_name" value="<?=htmlentities($last_name)?>" placeholder="Efternamn">
+                            </div>
+                          </div>
+                          <div id="form-row" class="form-row">
+                            <div class="col-6">
+                              <input type="text" class="form-control" name="email" value="<?=htmlentities($email)?>" placeholder="Email">
+                            </div>
+                            <div class="col-6">
+                              <input type="text" class="form-control" name="password" placeholder="Lösenord">
+                            </div>
+                          </div>
+                          <div id="form-row" class="form-row">
+                            <div class="col-6">
+                              <input type="text" class="form-control" name="phone" value="<?=htmlentities($phone)?>" placeholder="Telefon nr">
+                            </div>
+                            <div class="col-6">
+                              <input type="text" class="form-control" name="street" value="<?=htmlentities($street)?>" placeholder="Gatuaddres">
+                            </div>
+                          </div>
+                          <div id="form-row" class="form-row">
+                            <div class="col-6">
+                              <input type="text" class="form-control" name="postal_code" value="<?=htmlentities($postal_code)?>" placeholder="Postnummer">
+                            </div>
+                            <div class="col-6">
+                              <input type="text" class="form-control" name="city" value="<?=htmlentities($city)?>" placeholder="Stad">
+                            </div>
+                          </div>
+                          <div id="form-row" class="form-row">
+                            <div class="col-6">
+                              <input type="text" class="form-control" name="country" value="<?=htmlentities($country)?>" placeholder="Land">
+                            </div>
+                          </div>
+                            <div class="row justify-content-center">
+                                <div>     
+                                        <button type="submit" name="register" class="btn btn-success" value="register">Skapa</button>
+                                        <a class="btn btn-info" href="index.php" role="button">Tillbaka</a>      
+                                </div>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+            
+
+
+
+
+    </body>
+    </html>
+
---
+++ public/admin/index.php
@@ -0,0 +1,118 @@
+<?php
+
+	require('../../src/dbconnect.php');
+    
+
+
+
+
+
+	try {
+	    $query = "
+	        SELECT * FROM users
+	    ";
+
+	    $stmt = $dbconnect->query($query);
+	    $users = $stmt->fetchAll();
+	} catch (\PDOException $e) {
+	    throw new \PDOException($e->getMessage(), (int) $e->getCode());
+	}
+
+
+    if (isset($_POST['deleteUserBtn'])) {
+        $id = $_POST['userId'];
+
+        try {
+            $query = "
+                DELETE FROM users
+                WHERE id = :id;
+            ";
+            $stmt = $dbconnect->prepare($query); 
+            $stmt->bindValue(":id", $id);
+            $stmt->execute();
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int) $e->getCode());
+        }
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+	<h1 style="text-align: center;">Användare</h1>
+
+	<div id="content" style="">
+        <article class="admin-page">
+        
+        <a class="btn btn-success ml-1 mb-3" href="add_user.php" role="button">Skapa ny Användare</a>
+           
+            <table class="table table-striped">
+              <thead style="background-color: #67539E; color: white; margin-top: 50px;">
+                <tr>  
+                  <th scope="col">#Id</th>
+                  <th scope="col">Förnamn</th>
+                  <th scope="col">Efternamn</th>
+                  <th scope="col">E-mail</th>
+                  <th scope="col">Telefon nr</th>
+                  <th scope="col">Gatuaddres</th>
+                  <th scope="col">Postkod</th>
+                  <th scope="col">Stad</th>
+                  <th scope="col">Land</th>
+                  <th scope="col"></th>
+                  <th scope="col"></th>
+
+                </tr>
+
+              </thead>
+                
+                <?php foreach ($users as $key => $user) { ?>
+              <tbody>
+
+                <tr>
+                
+                  <th scope="row"><?=htmlentities($user['id'])?></th>
+                  <td><?=htmlentities($user['first_name'])?></td>
+                  <td><?=htmlentities($user['last_name'])?></td>
+                  <td><?=htmlentities($user['email'])?></td>
+                  <td><?=htmlentities($user['phone'])?></td>
+                  <td><?=htmlentities($user['street'])?></td>
+                  <td><?=htmlentities($user['postal_code'])?></td>
+                  <td><?=htmlentities($user['city'])?></td>
+                  <td><?=htmlentities($user['country'])?></td>
+                  <td>
+                      <form action="update_user.php" method="GET">
+                            <input type="hidden" name="id" value="<?=$user['id']?>">
+                            <button type="submit" name="register" class="btn btn-info" value="register">Redigera</button>
+                     </form>
+                  </td>
+                  <td>
+                      <form action="" method="POST">
+                            <input type="hidden" name="userId" value="<?=$user['id']?>">
+                            <button type="submit" name="deleteUserBtn" class="btn btn-danger" value="register">Radera</button>
+                     </form>
+
+                     
+
+                  </td>
+                </tr>
+
+                   
+
+
+              </tbody>
+              <?php } ?>
+            </table>
+          
+
+        </article>
+
+
+
+</body>
+</html>
