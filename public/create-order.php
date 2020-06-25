491306e2f69a7f7e92c8157c5c576217ccc6552f
Azaaxin <ludvig_olausson@hotmail.com>
2020-06-25 10:44:24 +0200

    fixed add to database //ludde

--- public/create-order.php
+++ public/create-order.php
@@ -3,7 +3,8 @@
 echo "<pre>";
 print_r($_POST);
 echo "</pre>";
-
+session_start();
+    error_reporting(-1);
 	
 	require('../src/dbconnect.php');
 
@@ -11,10 +12,10 @@ echo "</pre>";
 	print_r($_SESSION["products_shopping"]);
 	echo "<pre>";
     
+    $totalPrice = "";
 
 
-
-if (isset($_POST['createOrderBtn']));{
+if (!empty($_POST['createOrderBtn']));{
 	$firstName 		= trim($_POST['firstName']);
     $lastName 		= trim($_POST['lastName']);
     $email 			= trim($_POST['email']);
@@ -71,7 +72,7 @@ if (isset($_POST['createOrderBtn']));{
     try {
         $query = "
             INSERT INTO orders (user_id, total_price, billing_full_name, billing_street, billing_postal_code, billing_city, billing_country)
-            VALUES (:userId, :last_name, :phone, :street, :postal_code, :city, :country);
+            VALUES (:userId, :totalPrice, :fullName, :street, :postalCode, :city, :country);
         ";
 
         $stmt = $dbconnect->prepare($query);
@@ -89,48 +90,49 @@ if (isset($_POST['createOrderBtn']));{
     }
 
 
-    try {
-        $query = "
-            INSERT INTO orders (user_id, total_price, billing_full_name, billing_street, billing_postal_code, billing_city, billing_country)
-            VALUES (:userId, :last_name, :phone, :street, :postal_code, :city, :country);
-        ";
+    // try {
+    //     $query = "
+    //         INSERT INTO orders (user_id, total_price, billing_full_name, billing_street, billing_postal_code, billing_city, billing_country)
+    //         VALUES (:userId, :totalPrice, :fullName, :street, :postalCode, :city, :country);
+    //     ";
 
-        $stmt = $dbconnect->prepare($query);
-        $stmt->bindValue(':userId', $userId);
-        $stmt->bindValue(':totalPrice', $totalPrice);
-        $stmt->bindValue(':fullName', "{$firstName} {$lastName}");
-        $stmt->bindValue(':street', $street);
-        $stmt->bindValue(':postalCode', $postalCode);
-        $stmt->bindValue(':city', $city);
-        $stmt->bindValue(':country', $country);
-        $stmt->execute();
-        $orderId = $dbconnect->lastInsertId();
-    } catch(\PDOException $e) {
-        throw new \PDOException($e->getMessage(), (int) $e->getCode());
-    }
+    //     $stmt = $dbconnect->prepare($query);
+    //     $stmt->bindValue(':userId', $userId);
+    //     $stmt->bindValue(':totalPrice', $totalPrice);
+    //     $stmt->bindValue(':fullName', "{$firstName} {$lastName}");
+    //     $stmt->bindValue(':street', $street);
+    //     $stmt->bindValue(':postalCode', $postalCode);
+    //     $stmt->bindValue(':city', $city);
+    //     $stmt->bindValue(':country', $country);
+    //     $stmt->execute();
+    //     $orderId = $dbconnect->lastInsertId();
+    // } catch(\PDOException $e) {
+    //     throw new \PDOException($e->getMessage(), (int) $e->getCode());
+    // }
     
     $product_array = $_SESSION["products_shopping"];
 	foreach($product_array as $key=>$val)
 		try {
 	        $query = "
-	            INSERT INTO order_items (order_id, product_id, quantity, unit_price, product_title)
-	            VALUES (:orderId, :productId, :quantity, :price, :title);
+	            INSERT INTO order_items (order_id, product_id, unit_price, product_title)
+	            VALUES (:orderId, :productId, :price, :title);
 	        ";
 
 	        $stmt = $dbconnect->prepare($query);
 	        $stmt->bindValue(':orderId', $userId);
 	        $stmt->bindValue(':productId', $val->id);
-	        $stmt->bindValue(':quantity', $val->quantity);
+	       // $stmt->bindValue(':quantity', $val->quantity);
 	        $stmt->bindValue(':price', $val->price);
 	        $stmt->bindValue(':title', $val->title);
-	        $stmt->execute();
+            $stmt->execute();
+            
 	    } catch(\PDOException $e) {
 	        throw new \PDOException($e->getMessage(), (int) $e->getCode());
 	    }
 
-/*	unset($_SESSION["products_shopping"]); 
+    unset($_SESSION["products_shopping"]); 
 	header('location: order-confirmation.php');
-	exit;*/
+	exit;
    
 	} 
 
